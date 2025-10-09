95e4bebf928944af73a82bfa2292fe76baeb71e9
vadim <v@gdf.com>
2025-10-09 20:52:00 +0300

    script.js

--- src/frontend/js/script.js
+++ src/frontend/js/script.js
@@ -11,22 +11,24 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Добавление обработчиков событий для фильтров категорий
-    document.querySelectorAll('.category-list a').forEach(link => {
+    const categoryLinks = document.querySelectorAll('.category-list a');
+    // console.log('categoryLinks', categoryLinks)
+    categoryLinks.forEach(link => {
+        // console.log(link.getAttribute('data-category'));
         link.addEventListener('click', (e) => {
-            e.preventDefault();
+            // console.log(e)
             const category = e.target.getAttribute('data-category');
-            console.log(`Фильтр по категории: ${category}`);
-            // Реализация фильтрации по категориям (добавить позже)
-        });
+            // console.log(category)
+            fetchWallpapers(category)
+        })
     });
 
-    // Добавление обработчика событий для фильтра ориентации
-    document.getElementById('orientation').addEventListener('change', (e) => {
-        const orientation = e.target.value;
-        console.log(`Фильтр по ориентации: ${orientation}`);
-        // Реализация фильтрации по ориентации (добавить позже)
-    });
+    // // Добавление обработчика событий для фильтра ориентации
+    // document.getElementById('orientation').addEventListener('change', (e) => {
+    //     const orientation = e.target.value;
+    //     console.log(`Фильтр по ориентации: ${orientation}`);
+    //     // Реализация фильтрации по ориентации (добавить позже)
+    // });
 
     // Закрытие полноэкранного режима при клике за пределами изображения
     document.getElementById('fullscreen-box').addEventListener('click', (e) => {
@@ -36,33 +38,47 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-function fetchWallpapers() {
-    fetch('http://127.0.0.1:5000/api/wallpapers')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data.error) {
-                console.error('Ошибка при загрузке обоев:', data.error);
-                return;
-            }
-            const container = document.getElementById('wallpapers-container');
-            container.innerHTML = ''; // Очистка содержимого
-            data.wallpapers.forEach(wallpaper => {
-                const item = document.createElement('div');
-                item.className = 'wallpaper-item';
-                const imagePath = wallpaper.startsWith('wallpapers/') ? wallpaper : `wallpapers/${wallpaper}`;
-                item.innerHTML = `
-                    <img class="wallpaper-item-img" src="../${imagePath}" alt="Обои">
-                    <div class="wallpaper-menu"></div>
-                `;
-                container.appendChild(item);
-            });
-        })
-        .catch(error => console.error('Ошибка запроса:', error));
+async function fetchWallpapers(category = null) {
+    const orientation = document.getElementById('orientation').value;
+    const params = new URLSearchParams();
+    if (category) params.append('category', category);
+    if (orientation) params.append('orientation', orientation);
+    const response = await fetch(
+        `http://127.0.0.1:5000/api/wallpapers?${params.toString()}`);
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json()
+    console.log(data)
+
+    // fetch('http://127.0.0.1:5000/api/wallpapers')
+    //     .then(response => {
+    //         if (!response.ok) {
+    //             throw new Error(`HTTP error! status: ${response.status}`);
+    //         }
+    //         return response.json();
+    //     })
+    //     .then(data => {
+    //         if (data.error) {
+    //             console.error('Ошибка при загрузке обоев:', data.error);
+    //             return;
+    //         }
+    //         const container = document.getElementById('wallpapers-container');
+    //         container.innerHTML = ''; // Очистка содержимого
+    //         data.wallpapers.forEach(wallpaper => {
+    //             const item = document.createElement('div');
+    //             item.className = 'wallpaper-item';
+    //             const imagePath = wallpaper.startsWith('wallpapers/') ? wallpaper : `wallpapers/${wallpaper}`;
+    //             item.innerHTML = `
+    //                 <img class="wallpaper-item-img" src="../${imagePath}" alt="Обои">
+    //                 <div class="wallpaper-menu"></div>
+    //             `;
+    //             container.appendChild(item);
+    //         });
+    //     })
+    //     .catch(error => console.error('Ошибка запроса:', error));
 }
 
 function openFullScreen(imageSrc) {
