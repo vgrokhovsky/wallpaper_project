3581f16355a007d84a0eae6e3d6a6c4e58f3ef74
Morgarinka <freeswolf@gmail.com>
2025-10-26 20:49:00 +0300

    Проверка на наличие файла

--- src/backend/app/blueprints/image/routes.py
+++ src/backend/app/blueprints/image/routes.py
@@ -5,9 +5,16 @@ from flask import Blueprint, flash, jsonify, redirect, render_template, request,
 
 from ..main.models import Image
 from . import image_bp
+from werkzeug.utils import secure_filename
+from datetime import datetime
 
 WALLPAPERS_DIR = Path("src/frontend/wallpapers")
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg',}
 
+
+def allowed_file(filename):
+    """Проверка  расширений файла."""
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @image_bp.route("/api/image", methods=["GET", "POST"])
 def image_add():
@@ -18,7 +25,33 @@ def image_add():
             return redirect(request.url)
 
         file = request.files["file"]
+        #Проверка на корректность имени
+        if file.filename == '':
+            flash("Файл не выбран", "error")
+            return redirect(request.url)
+        
+          # Проверка на разрешенные форматы
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = WALLPAPERS_DIR / filename
 
-        flash("Image add")
-        return redirect(url_for("image_bp.image_add"))
+            WALLPAPERS_DIR.mkdir(parents=True, exist_ok=True)
+            file.save(filepath)
+
+            
+            new_image = Image(filename=filename, uploaded_at=datetime.utcnow())
+            db.session.add(new_image) 
+            db.session.commit() 
+
+            flash("Изображение успешно загружено и сохранено в базе данных!", "success")
+            return redirect(url_for("image_bp.image_add"))
+        else:
+            flash("Недопустимый формат файла", "error")
+            return redirect(request.url)
+
     return render_template("image_add.html")
+
+
+       # flash("Image add")
+       # return redirect(url_for("image_bp.image_add"))
+   # return render_template("image_add.html")
